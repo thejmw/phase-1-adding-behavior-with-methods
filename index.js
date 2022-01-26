ea445100fe9cd79655fe4df0978048fd22fe2f0d
Jason Williams <thejasonmichaelwilliams@gmail.com>
2022-01-26 09:56:08 -0500

    commit tree 04c05488e4bb8a77d603c6120725b9812bd0da08

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+class Dog{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says woof!`;
+    }
+}
+
+class Cat{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says meow!`;
+    }
+}
+
+
+class Bird{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return this.sex === 'male' ? `It\'s me! ${this.name}, the parrot!` : `${this.name} says squawk!`
+    }
+}
